2126ded8810bb5a1658744108da9c677dfafd868
Brian Perry <contact.brianperry@gmail.com>
2020-03-30 10:22:31 -0500

    Updated install script to set sitename

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 lando start
 lando composer install
-lando drush site:install -y
+lando drush site:install --site-name="Olivero Sandbox" -y
 lando drush then olivero
 lando drush config-set system.theme default olivero -y
 lando drush en styleguide

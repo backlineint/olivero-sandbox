32b1dd8903f5e54edc4ff05f0ed70bb7b6f6e3e3
Brian Perry <contact.brianperry@gmail.com>
2020-06-01 08:35:43 -0500

    Updates to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,9 @@
 
 lando start
 lando composer install
+# lando drush site:install minimal --site-name="Olivero Sandbox" -y
 lando drush site:install --site-name="Olivero Sandbox" -y
 lando drush then olivero
 lando drush config-set system.theme default olivero -y
-lando drush en styleguide
+lando drush upwd admin admin
+# lando drush en styleguide
